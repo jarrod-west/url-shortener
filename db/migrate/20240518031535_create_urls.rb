23c3078b11d5604c0ef604a063a09e7c1a35b6c7
Jarrod West <jnw.dev@gmail.com>
2024-05-18 13:37:14 +1000

    URL model

---
+++ db/migrate/20240518031535_create_urls.rb
@@ -0,0 +1,12 @@
+class CreateUrls < ActiveRecord::Migration[7.1]
+  def change
+    create_table :urls do |t|
+      t.string :original
+      t.string :short
+
+      t.timestamps
+    end
+
+    add_index :urls, :short, unique: true
+  end
+end
