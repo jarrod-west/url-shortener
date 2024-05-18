9b82fdf2c644a350a481c147f1e96aab8f352948
Jarrod West <jnw.dev@gmail.com>
2024-05-19 01:20:48 +1000

    Add copy success indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,15 @@
 
 // Create the clipboard object after the page has loaded
 $(document).ready(function(){
-  let clipboard = new Clipboard('.clipboard-btn');
+  let clipboard = new Clipboard('.copy-button');
 
   // Don't highlight the text
   clipboard.on('success', function(event) {
     event.clearSelection();
   });
 });
+
+function copyClicked(event) {
+  // Change the text to indicate the value was copied
+  event.value = "Copied!";
+}
